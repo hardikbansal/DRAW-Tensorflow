cfaeb54261f9b1395522fbca3f7742ec3f261fd7
Hardik Bansal <hardikbansal24@gmail.com>
2017-05-27 19:56:15 +0530

    Added the sigmod op

--- ops.py
+++ ops.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def flat_batch(input_batch, batch_size, final_width, final_height):
-	if(final_height*final_width != batch_size):		
+	if(final_height*final_width != batch_size):
 		print("Error flattening batch size. batch_size != final_width*final_height")
 	else:
 		h = 28
@@ -14,4 +14,6 @@ def flat_batch(input_batch, batch_size, final_width, final_height):
 			output[j*h:j*h+h, i*w:i*w+w] = image.reshape((w,h))
 
 	return output
-				
+
+def sigmoid(z):
+	return 1.0/(1.0+np.exp(-1.0*z))
